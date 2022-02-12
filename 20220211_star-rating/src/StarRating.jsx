aefd02edee924a7ec5c3f78d48cf8bff65ddda10
yuya0715 <negochan87@gmail.com>
2022-02-12 22:07:07 +0900

    途中

--- 20220211_star-rating/src/StarRating.jsx
+++ 20220211_star-rating/src/StarRating.jsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState }  from "react";
 import { FaStar } from "react-icons/fa";  // モジュールをinstallしないと使用できません
 
 export const StarRating = () => {
+
+  const [count, setCount] = useState(3)
+
   return (
     <>
-      <FaStar color="red" />
-      <FaStar color="red" />
-      <FaStar color="red" />
-      <FaStar color="red" />
-      <FaStar color="red" />
+      <FaStar color="red" onClick={() => setCount(1)} />
+      <FaStar color={count >1 ? "red" : "gray"} onClick={() => setCount(2)} />
+      <FaStar color={count >2 ? "red" : "gray"} onClick={() => setCount(3)} />
+      <FaStar color={count >3 ? "red" : "gray"} onClick={() => setCount(4)} />
+      <FaStar color={count >4 ? "red" : "gray"} onClick={() => setCount(5)} />
       <p>
-        {/* [赤色の数をここに出力] of 5 stars */}
+         {count} of 5 stars
+         
       </p>
     </>
   );
